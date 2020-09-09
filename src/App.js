2c07521c7c6fe1c88346929b64f2125a35d561b5
Martin Varela <martin.varelagarcia@plexus.es>
2020-09-09 13:05:47 +0200

    Configuration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Switch,  Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Login from './components/login/login';
 import Register from './components/register/register';
-
+import Home from './components/home/home';
 
 function App() {
-    return (
-        <div>
-            <Switch>
-                <Route path="/login" component={Login}/>
-                <Route path="/register" component={Register}/>
-            </Switch>
-        </div>
-    )
+  return (
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/home" component={Home} />
+    </Switch>
+  );
 }
 
 export default App;
