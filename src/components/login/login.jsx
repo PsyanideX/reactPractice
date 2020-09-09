73d812258b41bcef588fbc233a9cae5d91768b6f
Martin Varela <martin.varelagarcia@plexus.es>
2020-09-09 13:07:12 +0200

    login and register (no redirect yet)

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,30 +1,99 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { usernameValidation, passwordValidation } from '../../shared/validators/validators';
+import { apiUrl, getRequest } from '../../shared/constants/constants';
+import './login.css';
 
-class Login extends Component {
-  submitFormHandler = event => {
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      formControls: {
+        username: {
+          value: '',
+        },
+        password: {
+          value: '',
+        },
+      },
+    };
+  }
+  submitRegisterFormHandler = event => {
     event.preventDefault();
-    console.log(this.refs.username.value);
+    const isValidUsername = usernameValidation('username', this.state.formControls.username.value);
+    const isValidPasswword = passwordValidation('password', this.state.formControls.password.value);
+    let username = this.state.formControls.username.value;
+    let password = this.state.formControls.password.value;
+    if (!isValidUsername && !isValidPasswword) {
+      fetch(`${apiUrl}/users/?username=${this.state.formControls.username.value}`, { ...getRequest })
+        .then(response => response.json())
+        .then(response => {
+          if (response[0].username === username && response[0].password === password) {
+            console.log('LOGIN');
+          }
+        });
+    } else {
+      console.log(isValidUsername);
+      console.log(isValidPasswword);
+    }
+  };
+
+  changeHandler = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      formControls: {
+        ...this.state.formControls,
+        [name]: {
+          ...this.state.formControls[name],
+          value,
+        },
+      },
+    });
   };
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.submitFormHandler}>
-          <fieldset>
-            <legend>Login</legend>
-            <input type="text" placeholder="Username" ref="username" />
-            <input type="text" placeholder="Password" ref="password" />
-            <button type="submit">Login</button>
-          </fieldset>
+      <div className="container">
+        <form onSubmit={this.submitRegisterFormHandler} autoComplete="off">
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              placeholder=""
+              value={this.state.formControls.username.value}
+              name="username"
+              onChange={this.changeHandler}
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              placeholder="Password"
+              ref={this.state.formControls.password.value}
+              name="password"
+              onChange={this.changeHandler}
+              className="form-control"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+          <p>Already have an account?</p>
+          <Link to="/register">
+            <button type="button" className="btn btn-secondary">
+              Register
+            </button>
+          </Link>
         </form>
-        <p>Need an account?</p>
-        <Link to="/register">
-          <button type="button">Register</button>
-        </Link>
       </div>
     );
   }
 }
 
-export default Login;
+export default Register;
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,24 +1,118 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { usernameValidation, passwordValidation, emailValidation } from '../../shared/validators/validators';
+import { apiUrl, postRequest } from '../../shared/constants/constants';
+import './register.css';
 
-let registerTemplate = 
-    <div>
-        <fieldset>
-            <legend>Register</legend>
-            <input type="text" placeholder="Username"/>
-            <input type="text" placeholder="Password"/>
-            <button type="submit">Register</button>
-        </fieldset>
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      formControls: {
+        email: {
+          value: '',
+        },
+        username: {
+          value: '',
+        },
+        password: {
+          value: '',
+        },
+      },
+    };
+  }
+  submitRegisterFormHandler = event => {
+    event.preventDefault();
+    const isValidUsername = usernameValidation('username', this.state.formControls.username.value);
+    const isValidPasswword = passwordValidation('password', this.state.formControls.password.value);
+    const isValidEmail = emailValidation('email', this.state.formControls.email.value);
+    if (!isValidEmail && !isValidUsername && !isValidPasswword) {
+      const body = JSON.stringify({
+        username: this.state.formControls.username.value,
+        email: this.state.formControls.email.value,
+        password: this.state.formControls.password.value,
+      });
+      fetch(`${apiUrl}/users`, { ...postRequest, body: body }).then(response => {
+        if (response.status === 201) {
+          console.log('User added');
+        }
+        return response.json();
+      });
+    } else {
+      console.log(isValidEmail);
+      console.log(isValidUsername);
+      console.log(isValidPasswword);
+    }
+  };
+
+  changeHandler = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      formControls: {
+        ...this.state.formControls,
+        [name]: {
+          ...this.state.formControls[name],
+          value,
+        },
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.submitRegisterFormHandler} autoComplete="off">
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              placeholder="Email"
+              value={this.state.formControls.email.value}
+              name="email"
+              onChange={this.changeHandler}
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              placeholder="Username"
+              value={this.state.formControls.username.value}
+              name="username"
+              onChange={this.changeHandler}
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              ref={this.state.formControls.password.value}
+              name="password"
+              onChange={this.changeHandler}
+              required
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+        </form>
         <p>Already have an account?</p>
         <Link to="/login">
-            <button type="button">
-                Login
-            </button>
+          <button type="button" className="btn btn-secondary">
+            Login
+          </button>
         </Link>
-    </div>;
-
-function Register() {
-    return registerTemplate;
+      </div>
+    );
+  }
 }
 
 export default Register;
