d21b57c044a278209476500e8e2c0768735e7747
Martin Varela <martin.varelagarcia@plexus.es>
2020-09-15 09:44:39 +0200

    products refactored from class to functional component

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,68 +1,61 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { apiUrl, getRequest } from '../../shared/constants/constants';
 import Navbar from '../navbar/navbar';
 import Footer from '../footer/footer';
 import './products.css';
 
-class Products extends Component {
-  constructor() {
-    super();
-    this.state = {
-      department: '',
-      products: [],
-    };
-  }
+const Products = props => {
+  const [department, setDepartment] = useState('');
+  const [products, setProducts] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     let searchQuery = new URLSearchParams(window.location.search).get('search');
     if (searchQuery) {
       let url = `${apiUrl}/products/?productdescription_like=${searchQuery}`;
       fetch(url, getRequest)
         .then(response => response.json())
-        .then(response => this.setState({ products: response }));
+        .then(response => setProducts(response));
     } else {
-      let department = this.props.match.params.department;
-      this.setState({ department: department });
+      let department = props.match.params.department;
+      setDepartment(department);
       department = department ? department.charAt(0).toUpperCase() + department.slice(1).toLowerCase() : null;
       let url = department ? `${apiUrl}/products/?department=${department}` : `${apiUrl}/products`;
       fetch(url, getRequest)
         .then(response => response.json())
-        .then(response => this.setState({ products: response }));
+        .then(response => setProducts(response));
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="flex-wrapper">
-        <Navbar />
-        <div className="container">
-          <h1 class="products__departmentheader">{this.state.department}</h1>
-          <div className="row products__products">
-            {this.state.products.length > 0 ? (
-              this.state.products.map(product => (
-                <div className="card" key={product.productname}>
-                  <img src="https://www.w3schools.com/howto/img_avatar2.png" alt={product.productname} height="180px" />
-                  <div className="card-body">
-                    <Link to={`/product/${product.id}`}>
-                      <h5 className="card-title">{product.productname}</h5>
-                    </Link>
-                    <p className="card-text description">{product.productdescription}</p>
-                    <div>
-                      <p className="card-text dealprice">{`${product.price} €`}</p>
-                    </div>
+  return (
+    <div className="flex-wrapper">
+      <Navbar />
+      <div className="container">
+        <h1 class="products__departmentheader">{department}</h1>
+        <div className="row products__products">
+          {products.length > 0 ? (
+            products.map(product => (
+              <div className="card" key={product.productname}>
+                <img src="https://www.w3schools.com/howto/img_avatar2.png" alt={product.productname} height="180px" />
+                <div className="card-body">
+                  <Link to={`/product/${product.id}`}>
+                    <h5 className="card-title">{product.productname}</h5>
+                  </Link>
+                  <p className="card-text description">{product.productdescription}</p>
+                  <div>
+                    <p className="card-text dealprice">{`${product.price} €`}</p>
                   </div>
                 </div>
-              ))
-            ) : (
-              <h1>No hay ningún producto que coincida con la búsqueda, lo sentimos.</h1>
-            )}
-          </div>
+              </div>
+            ))
+          ) : (
+            <h1>No hay ningún producto que coincida con la búsqueda, lo sentimos.</h1>
+          )}
         </div>
-        <Footer />
       </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+  );
+};
 
 export default Products;
