b4077975c1a0ba9ab97f18636aa6546a193d41af
Martin Varela <martin.varelagarcia@plexus.es>
2020-09-15 09:35:27 +0200

    product refactored from class to functional component

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,104 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { apiUrl, getRequest } from '../../shared/constants/constants';
 import Navbar from '../navbar/navbar';
 import Footer from '../footer/footer';
 import './product.css';
 
-class Product extends Component {
-  constructor() {
-    super();
-    this.state = {
-      quantity: 1,
-      product: {
-        id: 0,
-        productname: '',
-        productdescription: '',
-        price: '',
-        department: '',
-        image: '',
-      },
-      avatars: [
-        'https://www.w3schools.com/howto/img_avatar2.png',
-        'https://www.w3schools.com/howto/img_avatar.png',
-        'https://www.w3schools.com/w3images/avatar2.png',
-      ],
-    };
-  }
+const Product = props => {
+  const [quantity, setQuantity] = useState(1);
+  const [product, setProduct] = useState({
+    id: 0,
+    productname: '',
+    productdescription: '',
+    price: '',
+    department: '',
+    image: '',
+  });
+  const [avatars, setAvatars] = useState([
+    'https://www.w3schools.com/howto/img_avatar2.png',
+    'https://www.w3schools.com/howto/img_avatar.png',
+    'https://www.w3schools.com/w3images/avatar2.png',
+  ]);
 
-  addOneUnity = () => {
-    this.setState({ quantity: this.state.quantity + 1 });
+  const addOneUnity = () => {
+    setQuantity(quantity + 1);
   };
 
-  substractOneUnity = () => {
-    let newQuantity = this.state.quantity > 1 ? this.state.quantity - 1 : 1;
-    this.setState({ quantity: newQuantity });
+  const substractOneUnity = () => {
+    let newQuantity = quantity > 1 ? quantity - 1 : 1;
+    setQuantity(newQuantity);
   };
 
-  componentDidMount() {
-    let productId = this.props.match.params.productId;
+  useEffect(() => {
+    let productId = props.match.params.productId;
     const url = `${apiUrl}/products/${productId}`;
     fetch(url, getRequest)
       .then(response => response.json())
-      .then(response => this.setState({ product: response }));
-  }
+      .then(response => setProduct(response));
+  }, []);
 
-  render() {
-    return (
-      <div className="flex-wrapper">
-        <Navbar />
-        <div className="container product__container">
-          <div className="row">
-            {/*<img src="https://www.w3schools.com/howto/img_avatar2.png" alt={this.state.product.productname} className="product__image" />*/}
-            <div id="productImageCarousel" className="carousel slide col">
-              <div className="carousel-inner">
-                <div className="carousel-item active" key="avatar">
-                  <img
-                    src="https://www.w3schools.com/howto/img_avatar2.png"
-                    alt={this.state.product.productname}
-                    className="product__image d-block w-100"
-                  />
+  return (
+    <div className="flex-wrapper">
+      <Navbar />
+      <div className="container product__container">
+        <div className="row">
+          <div id="productImageCarousel" className="carousel slide col">
+            <div className="carousel-inner">
+              <div className="carousel-item active" key="avatar">
+                <img src="https://www.w3schools.com/howto/img_avatar2.png" alt={product.productname} className="product__image d-block w-100" />
+              </div>
+              {avatars.map((avatar, i) => (
+                <div className="carousel-item" key={`avatar${i}`}>
+                  <img src={avatar} alt={product.productname} className="product__image d-block w-100" />
                 </div>
-                {this.state.avatars.map((avatar, i) => (
-                  <div className="carousel-item" key={`avatar${i}`}>
-                    <img src={avatar} alt={this.state.product.productname} className="product__image d-block w-100" />
-                  </div>
-                ))}
-                <a className="carousel-control-prev" href="#productImageCarousel" role="button" data-slide="prev">
-                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#productImageCarousel" role="button" data-slide="next">
-                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span className="sr-only">Next</span>
-                </a>
-              </div>
+              ))}
+              <a className="carousel-control-prev" href="#productImageCarousel" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+              </a>
+              <a className="carousel-control-next" href="#productImageCarousel" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+              </a>
             </div>
-            <div className="product-details col">
-              <h2>{this.state.product.productname}</h2>
-              <p>Descripción del artículo:</p>
-              <p>{this.state.product.productdescription}</p>
-              <h4>{this.state.product.price}€</h4>
-              <div className="buy-buttons">
-                <button className="btn btn-default">Añadir a la cesta</button>
-                <button className="btn btn-default">Comprar</button>
-              </div>
-              <p>Cantidad:</p>
-              <div className="btn-group">
-                <button className="btn quantity-buttons" onClick={this.substractOneUnity}>
-                  <i className="far fa-minus-square"></i>
-                </button>
-                <input type="text" value={this.state.quantity} className="form-control product__quantity" />
-                <button className="btn quantity-buttons" onClick={this.addOneUnity}>
-                  <i className="far fa-plus-square"></i>
-                </button>
-              </div>
+          </div>
+          <div className="product-details col">
+            <h2>{product.productname}</h2>
+            <p>Descripción del artículo:</p>
+            <p>{product.productdescription}</p>
+            <h4>{product.price}€</h4>
+            <div className="buy-buttons">
+              <button className="btn btn-default">Añadir a la cesta</button>
+              <button className="btn btn-default">Comprar</button>
+            </div>
+            <p>Cantidad:</p>
+            <div className="btn-group">
+              <button className="btn quantity-buttons" onClick={substractOneUnity}>
+                <i className="far fa-minus-square"></i>
+              </button>
+              <input type="text" value={quantity} className="form-control product__quantity" />
+              <button className="btn quantity-buttons" onClick={addOneUnity}>
+                <i className="far fa-plus-square"></i>
+              </button>
             </div>
           </div>
         </div>
-        <Footer />
       </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+  );
+};
 
 export default Product;
