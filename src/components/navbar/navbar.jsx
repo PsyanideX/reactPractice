59e43822eb90462d15f30eb134bcdd4a66e231de
Martin Varela <martin.varelagarcia@plexus.es>
2020-09-17 14:07:55 +0200

    Menu on navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
+import { useDispatch } from 'react-redux';
+import { loggedOut } from '../../core/store/reducers/loginSlice';
+
 const Navbar = ({ onlyLogo = false }) => {
+  const dispatch = useDispatch();
+
+  const logOut = () => {
+    dispatch(loggedOut());
+  };
+
   return (
     <nav className="navbar row">
       <div className="col-xl-10 col-bg-9 col-md-8 col-sm-7 col-6">
@@ -20,10 +29,29 @@ const Navbar = ({ onlyLogo = false }) => {
               </button>
             </Link>
           </div>
-          <div className="col-xl-1 col-bg-2 col-md-2 col-sm-2 col-3">
-            <a className="navbar__avatar btn">
+
+          <div className="col-xl-1 col-bg-2 col-md-2 col-sm-2 col-3 dropdown">
+            <button
+              className="navbar__avatar btn"
+              type="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
               <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="avatar" />
-            </a>
+            </button>
+            <div className="dropdown-menu navbar__dropdown" aria-labelledby="dropdownMenuLink">
+              <Link to="/home" className="dropdown-item">
+                Favoritos
+              </Link>
+              <a className="dropdown-item" href="#">
+                Pedidos
+              </a>
+              <button className="dropdown-item button__logout" onClick={logOut} href="#">
+                Cerrar sesión
+              </button>
+            </div>
           </div>
         </React.Fragment>
       ) : null}
