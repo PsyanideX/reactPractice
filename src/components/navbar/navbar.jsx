ef4c215e7bbb24a6f18307d2bc97d7946178d639
Martin Varela <martin.varelagarcia@plexus.es>
2020-09-17 11:08:16 +0200

    now navbar has a prop to manage cart and avatar display

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ onlyLogo = false }) => {
   return (
     <nav className="navbar row">
       <div className="col-xl-10 col-bg-9 col-md-8 col-sm-7 col-6">
@@ -11,18 +11,22 @@ const Navbar = () => {
         </Link>
       </div>
 
-      <div className="col-xl-1 col-bg-1 col-md-2 col-sm-3 col-3">
-        <Link to="/cart">
-          <button className="navbar__cart btn btn-dark">
-            <i className="fas fa-shopping-cart"></i>
-          </button>
-        </Link>
-      </div>
-      <div className="col-xl-1 col-bg-2 col-md-2 col-sm-2 col-3">
-        <a className="navbar__avatar btn">
-          <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="avatar" />
-        </a>
-      </div>
+      {onlyLogo === false ? (
+        <React.Fragment>
+          <div className="col-xl-1 col-bg-1 col-md-2 col-sm-3 col-3">
+            <Link to="/cart">
+              <button className="navbar__cart btn btn-dark">
+                <i className="fas fa-shopping-cart"></i>
+              </button>
+            </Link>
+          </div>
+          <div className="col-xl-1 col-bg-2 col-md-2 col-sm-2 col-3">
+            <a className="navbar__avatar btn">
+              <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="avatar" />
+            </a>
+          </div>
+        </React.Fragment>
+      ) : null}
     </nav>
   );
 };
